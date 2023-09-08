506255b71e803570448dadd12d663539d8a0b6e6
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2023-09-08 21:26:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ let ultag = document.getElementById('ul');
 let add = document.getElementById('add');
 let remove = document.getElementById('remove');
 
-add.addEventListener('click', (e) => {
-
+let clicking_remove=function(){
+      let list=document.querySelectorAll('li')
+    list.forEach((list)=>{
+        list.addEventListener('click',(e)=>{
+            console.log(e.target.textContent);
+            e.target.remove();
+        })
+    })
+}
+add.addEventListener('click', (e) => { 
     let createli = document.createElement('li')
     createli.textContent = inputtag.value;
-    ultag.appendChild(createli)
+    ultag.appendChild(createli);
+    clicking_remove();
 })
 remove.addEventListener('click', (e) => {
     ultag.removeChild(ultag.children[0])
 })
+clicking_remove();
